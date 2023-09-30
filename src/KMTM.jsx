f94dc6e02e4d68bde6c4c656a2f88e936faa96b2
Riyan Satria <osx@supercomputer-2.local>
2023-09-30 14:51:34 +0700

    Updatean

--- src/KMTM.jsx
+++ src/KMTM.jsx
@@ -121,6 +121,19 @@ const BusinessMeeting = () => {
                         <div style={{display: 'flex',flexGrow: 1,flexDirection: 'column'}}>
                             <h3 style={{margin: 0,fontSize: 32}}>{seller.name}</h3>
                             <pre style={{fontSize: 14,marginTop: 20,lineHeight: '22px',fontFamily: "sans-serif",whiteSpace: 'pre-wrap',wordWrap: 'break-word'}}>{seller.description}</pre>
+
+                            <div style={{display: 'flex',flexDirection: 'row',gap: 20,borderBottomWidth: 1,borderBottomColor: '#ddd',borderBottomStyle: 'solid',padding: 20,alignItems: 'center'}}>
+                                <b>Specialize</b>
+                                <div>{seller.specialize}</div>
+                            </div>
+                            <div style={{display: 'flex',flexDirection: 'row',gap: 20,borderBottomWidth: 1,borderBottomColor: '#ddd',borderBottomStyle: 'solid',padding: 20,alignItems: 'center'}}>
+                                <b>Website</b>
+                                <div>
+                                    <a href={seller.website} target="_blank" style={{color: '#eb2f28'}}>
+                                        {seller.website}
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
